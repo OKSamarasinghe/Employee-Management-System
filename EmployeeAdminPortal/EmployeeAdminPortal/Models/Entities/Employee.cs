227072990edc82a229b4aace9000246211a64b4e
Oshadha Samarasinghe <124981711+OKSamarasinghe@users.noreply.github.com>
2025-05-21 14:37:10 +0530

    commit

---
+++ EmployeeAdminPortal/EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string? Phone { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Salary { get; set; }
+    }
+}
